89fecca530e676bc1a127f446d6510f95fbc9c2f
Arivalagan Mathiyalagu <arimathi123@gmail.com>
2025-03-14 14:12:09 +0000

    third

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var authRouter = require('./routes/auth');
 var app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["https://passwordsreset.netlify.app", "http://localhost:5173", "http://127.0.0.1:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
